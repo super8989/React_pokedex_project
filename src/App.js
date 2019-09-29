521b59fb23018317ad7610761a7819f500fde01f
Sam <super8989@gmail.com>
2019-09-28 21:11:09 -0400

    created pokecard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Pokecard from './Pokecard';
-import './App.css';
+import React from "react";
+import Pokecard from "./Pokecard";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Pokecard />      
+      <Pokecard id={4} name="Charmander" type="fire" exp={62} />
     </div>
   );
 }
